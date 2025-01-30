2572a690ef5eaebaab0d9cbf20e624d8b43ac583
VimukthiKolitha <vimukthikolitah@gmail.com>
2025-01-30 20:43:11 +0530

    php opertors

---
+++ phpLearning/opertor.php
@@ -0,0 +1,22 @@
+<?php 
+   
+   $x = 10;
+   $y = '10';
+
+   //this is goin to be check is values same
+   echo $x==$y;
+
+
+   //'===' this is call as identical operator
+   //!== this is call as not identical
+    $w = 10;
+    $r = 20;
+   //this is going to be check is value and data type are same?
+   echo $w===$r;//true
+
+   $i = 10;
+   $j = '20';
+
+  echo $i===$j;  //false
+
+?>
