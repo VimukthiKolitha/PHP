560ae6222a796aa27eb080594362431baabc57cc
VimukthiKolitha <vimukthikolitah@gmail.com>
2025-01-30 20:43:34 +0530

    veriables

---
+++ phpLearning/variableCreation.php
@@ -0,0 +1,20 @@
+<?php
+  //veriable create ($x)
+  $x = 10;
+  $y = 30;
+
+  $total = $x + $y;
+
+  echo "$total";
+
+  $name = 'vimukthi';
+  $age = 29;
+
+  echo "<br> $name is $age years old"; 
+
+  //this is corsing error
+      //echo "i am " + $age;
+
+      //this is work fine
+      echo " <br>i am " . $age;
+?>
